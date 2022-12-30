cec490976ec174ca500c7d29e41a093216e489fd
FelipeCruz17 <feemattioli@gmail.com>
2022-12-30 17:15:30 -0300

    refactor: ***time-intervals***

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,7 +7,7 @@ import {
   Text,
   TextInput,
 } from '@ignite-ui/react'
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -88,7 +88,7 @@ export default function TimeIntervals() {
     },
   })
 
-  // const router = useRouter()
+  const router = useRouter()
 
   const weekDays = getWeekDays()
 
@@ -106,7 +106,7 @@ export default function TimeIntervals() {
       intervals,
     })
 
-    // await router.push('/register/update-profile')
+    await router.push('/register/update-profile')
   }
 
   return (
